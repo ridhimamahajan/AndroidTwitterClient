a598158c82d04837b115aad7c2e2187c07cf327f
Ridhima Mahajan <ridmahaj@indiana.edu>
2013-09-10 20:17:49 -0700

    Adding functionality for a basic twitter client on android

---
+++ src/com/apps/androidtwitterapp/ComposeActivity.java
@@ -0,0 +1,70 @@
+package com.apps.androidtwitterapp;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+
+import com.apps.androidtwitterapp.models.Tweet;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class ComposeActivity extends Activity {
+
+	EditText etTweet; 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_compose);
+		etTweet = (EditText) findViewById(R.id.etTweet);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.compose, menu);
+		return true;
+	}
+	
+	public void onTweet(View view) {
+		String tweetText= etTweet.getText().toString();
+		RequestParams params = new RequestParams();
+		params.put("status", tweetText );
+		TwitterClientApp.getRestClient().postTweet(params, new JsonHttpResponseHandler(){
+			@Override
+			public void onSuccess(JSONArray jsonTweets){
+				
+				Intent data = new Intent();
+				
+				data.putExtra("jsonTweets", jsonTweets.toString() );
+
+				if (getParent() == null) {
+				    setResult(Activity.RESULT_OK, data);
+				} else {
+				    getParent().setResult(Activity.RESULT_OK, data);
+				}
+				finish();
+
+			}
+//			@Override
+//			public void onFailure(JSONArray jsonTweets){
+//				super.onFailure();
+//			}
+
+		});
+		
+		
+	}
+	
+
+}
---
+++ src/com/apps/androidtwitterapp/TimelineActivity.java
@@ -0,0 +1,69 @@
+package com.apps.androidtwitterapp;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.apps.androidtwitterapp.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+public class TimelineActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timeline);
+		TwitterClientApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler(){
+			@Override
+			public void onSuccess(JSONArray jsonTweets){
+				updateAndDisplayTimeline(jsonTweets);
+			}
+
+			
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.timeline, menu);
+		return true;
+	}
+	
+	public void onComposeClick(MenuItem m){
+		Intent i = new Intent(getApplicationContext(), ComposeActivity.class);
+		startActivityForResult(i,1);
+		
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data){
+		if(resultCode == RESULT_OK && requestCode == 1 ){
+			JSONArray jsonTweets=null;
+			try{
+				jsonTweets= new JSONArray(data.getStringExtra("jsonArray"));
+			}
+			catch (JSONException e){
+				e.printStackTrace();
+			}
+			updateAndDisplayTimeline(jsonTweets);
+		}
+	}
+	public void updateAndDisplayTimeline(JSONArray jsonTweets) {
+		ArrayList<Tweet> tweet = Tweet.fromJson(jsonTweets);
+		ListView lvTweet = (ListView) findViewById(R.id.lvTweet);
+		TweetAdapter tweetAdapter = new TweetAdapter(getBaseContext(), tweet);
+		lvTweet.setAdapter(tweetAdapter);
+		Log.d("DEBUG", jsonTweets.toString());
+	}
+	
+
+}
